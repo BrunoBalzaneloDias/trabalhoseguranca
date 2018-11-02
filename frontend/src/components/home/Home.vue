2568d8a1f03f4a188973395f0bb35ba2b96ba887
BrunoBalzaneloDias <brunobbd98@gmail.com>
2018-11-02 14:14:49 -0300

    initial commit

---
+++ frontend/src/components/home/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="home">  
+        <div class="page-title">
+            <h1><i class="fa fa-home"></i> Dashboard</h1>
+            <hr>
+        </div> 
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { baseApiUrl } from '@/global'
+
+export default {
+    name: 'Home',
+    components: { },
+    data: function() {
+        return {}
+    },
+    methods: {
+    },
+    mounted() {
+        
+    }
+}
+</script>
+
+<style>
+
+    .page-title h1 {
+        margin: 0px;
+    }
+
+</style>
---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer class="footer">
+        <span class="trab">Trabalho de Segurança e Auditoria de Sistemas</span>
+        <span class="utf">UTFPR - Câmpus Cornélio Procópio</span>
+        
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #DDD;
+        color: #333;
+
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 10px;
+    }
+
+    .trab {
+        align-content: right;
+    }
+</style>
