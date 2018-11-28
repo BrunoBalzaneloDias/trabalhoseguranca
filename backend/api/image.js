d967fb88111441d3574790344c2a7566bb21798a
BrunoBalzaneloDias <44004608+BrunoBalzaneloDias@users.noreply.github.com>
2018-11-28 20:47:04 -0200

    Update image.js

--- backend/api/image.js
+++ backend/api/image.js
@@ -2,20 +2,11 @@ const fs = require('fs')
 const crypto = require('crypto')
 var mkdirp = require('mkdirp');
 
-const alg = 'aes-256-ctr'
-const passwd = 'abcdabcdabcdabcdabcdabcdabcdabcd'
 module.exports = app => {
 
     const save = (req, res) => {
         //console.log(req.body)
 
-
-        /*mkdirp.mkdirp('img/uploads/' + req.body.idDono+'/')
-        const iv = crypto.randomBytes(16)
-        const read = fs.createReadStream(req.body.path)
-        const write = fs.createWriteStream('img/'+req.body.path)
-        const cipher = crypto.createCipheriv(alg, passwd, iv)
-        read.pipe(cipher).pipe(write)*/
 
 
         const arquivo = { ...req.body }
